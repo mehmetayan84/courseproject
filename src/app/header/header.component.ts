c97c9157a16304d05a448f64f1f7e393d112256e
MehmetAyan <mehmetayan84@gmail.com>
2018-01-13 00:57:05 +0300

    routing is added to project

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { Output } from '@angular/core';
-import { EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,9 +6,4 @@ import { EventEmitter } from '@angular/core';
 })
 export class HeaderComponent {
 
-  @Output() viewSelection = new EventEmitter<string>();
-
-  onMenuSelect(input:string){
-    this.viewSelection.emit(input);
-  }
 }
